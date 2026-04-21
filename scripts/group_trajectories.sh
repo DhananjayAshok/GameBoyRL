a6b92b9162cef641f232b35d67a1d880745e50ca
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2026-04-21 09:47:04 -0700

    fix

--- scripts/group_trajectories.sh
+++ scripts/group_trajectories.sh
@@ -86,5 +86,5 @@ fi
 
 replay_buffer_save_folder="${ARGS["replay_buffer_folder"]}"
 cd cleanrl
-python cleanrl_utils/group_trajectories.py --replay_buffer_folder $storage_dir/replay_buffers/${ARGS["game"]}/$replay_buffer_save_folder --save_path $storage_dir/grouped_trajectories/${ARGS["game"]}/$replay_buffer_save_folder/ --z_min ${ARGS["z_min"]} --z_kind ${ARGS["z_kind"]}
+python cleanrl_utils/group_trajectories.py --replay_buffer_folder $storage_dir/replay_buffers/${ARGS["game"]}/$replay_buffer_save_folder --save_path ${ARGS["save_path"]} --z_min ${ARGS["z_min"]} --z_kind ${ARGS["z_kind"]}
 cd ..
