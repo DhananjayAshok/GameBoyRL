cffed0424ea96e88e87a9ea07e129eda6ab6a3eb
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2026-04-28 10:43:41 -0700

    Added all games to clean

--- scripts/unsafe_clean_all.sh
+++ scripts/unsafe_clean_all.sh
@@ -8,5 +8,9 @@ rm -rf curiosity_buffers models replay_buffers tmp
 cd $currpath
 source GameBoyWorlds/configs/config.env
 echo "Clearing sessions in GameBoyWorlds..."
-rm -rf $storage_dir/sessions/pokemon_red/pokemon_red_*
+games=("pokemon_red" "pokemon_crystal" "pokemon_brown" "pokemon_prism" "pokemon_fools_gold" "pokemon_starbeasts" "sword_of_hope_1" "sword_of_hope_2" "deja_vu_1" "deja_vu_2" "harvest_moon_1" "harvest_moon_2" "harvest_moon_3" "harrypotter_philosophersstone" "legend_of_zelda_links_awakening" "legend_of_zelda_the_oracle_of_seasons" "bomberman_pocket" "bomberman_quest" "bomberman_max")
+for game in "${games[@]}"; do
+    echo "Cleaning sessions for game: $game"
+    rm -rf $storage_dir/sessions/$game/${game}_*
+done
 source scripts/utils.sh
