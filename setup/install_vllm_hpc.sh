511cf394e69a58642cc3ba78999e68b9d00ca5aa
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2026-06-12 13:49:37 -0700

    Setup config

--- setup/install_vllm_hpc.sh
+++ setup/install_vllm_hpc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Sets up flash attention and vLLM installs on the specific GPU nodes. Must be run on the GPU node itself. 
 
-module load gcc/13.3.0 cuda/12.9.1
+module load ver/2506 gcc/14.3.0 cuda/12.9.1
 source setup/.venv/bin/activate
 uv pip install --force-reinstall --no-cache-dir torch==2.11.0 torchvision==0.26.0 --index-url https://download.pytorch.org/whl/cu129
 export FLASH_ATTENTION_FORCE_BUILD=TRUE
