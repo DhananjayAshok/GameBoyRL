8cd7e1a03167b80840e94822b38ed318b4318845
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2026-07-01 08:18:39 -0400

    Ok

---
+++ assets/improvement_data.js
@@ -0,0 +1,171 @@
+// Placeholder base vs improved success rates per series and game.
+window.IMPROVEMENT_DATA = [
+  {
+    "id": "bomberman",
+    "name": "Bomberman",
+    "games": [
+      {
+        "id": "bomberman_quest",
+        "name": "Bomberman Quest",
+        "base": 34.1,
+        "improved": 44.1
+      },
+      {
+        "id": "bomberman_pocket",
+        "name": "Bomberman Pocket",
+        "base": 26.3,
+        "improved": 37.5
+      },
+      {
+        "id": "bomberman_max",
+        "name": "Bomberman Max",
+        "base": 46.6,
+        "improved": 57.9
+      }
+    ]
+  },
+  {
+    "id": "deja_vu",
+    "name": "Deja Vu",
+    "games": [
+      {
+        "id": "deja_vu_1",
+        "name": "Deja Vu 1",
+        "base": 33.4,
+        "improved": 55.5
+      },
+      {
+        "id": "deja_vu_2",
+        "name": "Deja Vu 2",
+        "base": 38.9,
+        "improved": 67.4
+      }
+    ]
+  },
+  {
+    "id": "harry_potter",
+    "name": "Harry Potter",
+    "games": [
+      {
+        "id": "harry_potter_philosophers_stone",
+        "name": "Harry Potter Philosophers Stone",
+        "base": 37.1,
+        "improved": 52.6
+      },
+      {
+        "id": "harry_potter_chamber_of_secrets",
+        "name": "Harry Potter Chamber Of Secrets",
+        "base": 41.5,
+        "improved": 51.9
+      }
+    ]
+  },
+  {
+    "id": "harvest_moon",
+    "name": "Harvest Moon",
+    "games": [
+      {
+        "id": "harvest_moon_1",
+        "name": "Harvest Moon 1",
+        "base": 51.6,
+        "improved": 65.9
+      },
+      {
+        "id": "harvest_moon_2",
+        "name": "Harvest Moon 2",
+        "base": 33.6,
+        "improved": 55.7
+      },
+      {
+        "id": "harvest_moon_3",
+        "name": "Harvest Moon 3",
+        "base": 25.3,
+        "improved": 46.8
+      }
+    ]
+  },
+  {
+    "id": "legend_of_zelda",
+    "name": "Legend Of Zelda",
+    "games": [
+      {
+        "id": "legend_of_zelda_links_awakening",
+        "name": "Legend Of Zelda Links Awakening",
+        "base": 49.7,
+        "improved": 63.2
+      },
+      {
+        "id": "legend_of_zelda_the_oracle_of_seasons",
+        "name": "Legend Of Zelda The Oracle Of Seasons",
+        "base": 53.1,
+        "improved": 71.3
+      }
+    ]
+  },
+  {
+    "id": "pokemon",
+    "name": "Pokemon",
+    "games": [
+      {
+        "id": "pokemon_red",
+        "name": "Pokemon Red",
+        "base": 47.1,
+        "improved": 63.8
+      }
+    ]
+  },
+  {
+    "id": "runes_of_virtue",
+    "name": "Runes Of Virtue",
+    "games": [
+      {
+        "id": "runes_of_virtue_1",
+        "name": "Runes Of Virtue 1",
+        "base": 34.1,
+        "improved": 45.8
+      },
+      {
+        "id": "runes_of_virtue_2",
+        "name": "Runes Of Virtue 2",
+        "base": 20.3,
+        "improved": 30.4
+      }
+    ]
+  },
+  {
+    "id": "survival_kids",
+    "name": "Survival Kids",
+    "games": [
+      {
+        "id": "survival_kids_1",
+        "name": "Survival Kids 1",
+        "base": 54.6,
+        "improved": 73.0
+      },
+      {
+        "id": "survival_kids_2",
+        "name": "Survival Kids 2",
+        "base": 36.2,
+        "improved": 63.7
+      }
+    ]
+  },
+  {
+    "id": "sword_of_hope",
+    "name": "Sword Of Hope",
+    "games": [
+      {
+        "id": "sword_of_hope_1",
+        "name": "Sword Of Hope 1",
+        "base": 41.2,
+        "improved": 70.6
+      },
+      {
+        "id": "sword_of_hope_2",
+        "name": "Sword Of Hope 2",
+        "base": 32.5,
+        "improved": 42.9
+      }
+    ]
+  }
+];
